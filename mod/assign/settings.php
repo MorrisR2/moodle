netspotau/moodle-mod_assign

MDL-31288: New feature: Assignment submission statements

mod/assign/settings.php
@@ -50,11 +50,19 @@ if ($ADMIN->fulltree) {
 
     // The default here is feedback_comments (if it exists)
     $settings->add(new admin_setting_configselect('assign/feedback_plugin_for_gradebook',
-                   new lang_string('feedbackpluginforgradebook', 'mod_assign'),
-                   new lang_string('feedbackplugin', 'mod_assign'), 'assignfeedback_comments', $menu));
+                       new lang_string('feedbackpluginforgradebook', 'mod_assign'),
+                       new lang_string('feedbackplugin', 'mod_assign'), 'assignfeedback_comments', $menu));
     $settings->add(new admin_setting_configcheckbox('assign/showrecentsubmissions',
-                   new lang_string('showrecentsubmissions', 'assign'),
-                   new lang_string('configshowrecentsubmissions', 'assign'), 0));
+                       new lang_string('showrecentsubmissions', 'mod_assign'),
+                       new lang_string('configshowrecentsubmissions', 'mod_assign'), 0));
     $settings->add(new admin_setting_configcheckbox('assign/submissionreceipts',
-                   get_string('sendsubmissionreceipts', 'mod_assign'), get_string('sendsubmissionreceipts_help', 'mod_assign'), 1));
+                       new lang_string('sendsubmissionreceipts', 'mod_assign'),
+                       new lang_string('sendsubmissionreceipts_help', 'mod_assign'), 1));
+    $settings->add(new admin_setting_configtextarea('assign/submissionstatement',
+                       new lang_string('submissionstatement', 'mod_assign'),
+                       new lang_string('submissionstatement_help', 'mod_assign'), ''));
+    $settings->add(new admin_setting_configcheckbox('assign/requiresubmissionstatement',
+                       new lang_string('requiresubmissionstatement', 'mod_assign'),
+                       new lang_string('requiresubmissionstatement_help', 'mod_assign'), 0));
+
 }

MorrisR2/moodle

mod/assign/settings.php
@@ -50,11 +50,19 @@ if ($ADMIN->fulltree) {
 
     // The default here is feedback_comments (if it exists)
     $settings->add(new admin_setting_configselect('assign/feedback_plugin_for_gradebook',
-                   new lang_string('feedbackplugin', 'mod_assign'),
-                   new lang_string('feedbackpluginforgradebook', 'mod_assign'), 'assignfeedback_comments', $menu));
+                       new lang_string('feedbackplugin', 'mod_assign'),
+                       new lang_string('feedbackpluginforgradebook', 'mod_assign'), 'assignfeedback_comments', $menu));
     $settings->add(new admin_setting_configcheckbox('assign/showrecentsubmissions',
-                   new lang_string('showrecentsubmissions', 'assign'),
-                   new lang_string('configshowrecentsubmissions', 'assign'), 0));
+                       new lang_string('showrecentsubmissions', 'mod_assign'),
+                       new lang_string('configshowrecentsubmissions', 'mod_assign'), 0));
     $settings->add(new admin_setting_configcheckbox('assign/submissionreceipts',
-                   get_string('sendsubmissionreceipts', 'mod_assign'), get_string('sendsubmissionreceipts_help', 'mod_assign'), 1));
+                       new lang_string('sendsubmissionreceipts', 'mod_assign'),
+                       new lang_string('sendsubmissionreceipts_help', 'mod_assign'), 1));
+    $settings->add(new admin_setting_configtextarea('assign/submissionstatement',
+                       new lang_string('submissionstatement', 'mod_assign'),
+                       new lang_string('submissionstatement_help', 'mod_assign'), ''));
+    $settings->add(new admin_setting_configcheckbox('assign/requiresubmissionstatement',
+                       new lang_string('requiresubmissionstatement', 'mod_assign'),
+                       new lang_string('requiresubmissionstatement_help', 'mod_assign'), 0));
+
 }