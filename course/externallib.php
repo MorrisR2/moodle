950e90b43266b499f101bf7d1f0aac345aaad81a
Sam Hemelryk <sam@moodle.com>
2013-08-21 15:11:59 +1200

    Revert "MDL-39876 update_course_record function to update global COURSE"
    This reverts commit c290ac5510ca06b3eae557c2165c2f106dde46aa.
    
    Conflicts:
    	course/lib.php

--- course/externallib.php
+++ course/externallib.php
@@ -1077,7 +1077,7 @@ class core_course_external extends external_api {
         $course->visible = $params['visible'];
 
         // Set shortname and fullname back.
-        update_course_record($course);
+        $DB->update_record('course', $course);
 
         if (empty($CFG->keeptempdirectoriesonbackup)) {
             fulldelete($backupbasepath);
